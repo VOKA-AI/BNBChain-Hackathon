fe541586995d33deca7eeb35f1aa31f5fd6e43f2
vincent <aoxiaotian@foxmail.com>
2024-02-06 11:19:50 +0800

    config threejs ui

--- src/components/modelPreview/index.tsx
+++ src/components/modelPreview/index.tsx
@@ -23,6 +23,31 @@ function Rotate(props:any) {
   return <group ref={ref} {...props} />
 }
 
+/*
+ * 获取主要scene，用于primitive
+ */
+function getSceneFromModel(model:any) {
+  var scene = model.scene;
+  return scene;
+}
+
+export default function Model2({url = "", scale=3.8}) {
+  const model = useGLTF(url);
+  const scene = getSceneFromModel(model);
+//   var mesh = getMorphTargetMesh(model);
+
+  const objRef = useRef<any>();
+
+  return (
+    <group dispose={null} rotation={[0, 0, 0]} scale={scale} ref={objRef}>
+    <ambientLight intensity={0.1} />
+    <directionalLight position={[0, 5, 5]} />
+    <primitive object={scene} />
+    </group>
+    )
+  }
+
+
 
 
 export const ModelPreview = ({url = ""}) => {
@@ -30,14 +55,14 @@ export const ModelPreview = ({url = ""}) => {
     return (
         <>
             {<Suspense fallback={<span>loading...</span>}>
-                <Canvas dpr={[1, 2]} camera={{ position: [-2, 2, 4], fov: 25 }}>
+                <Canvas style={{height:600}} dpr={[1, 2]} camera={{ position: [70, 2, 4], fov: 25 }}>
                     <directionalLight position={[10, 10, 0]} intensity={1.5} />
                     <directionalLight position={[-10, 10, 5]} intensity={1} />
                     <directionalLight position={[-10, 20, 0]} intensity={1.5} />
                     <directionalLight position={[0, -10, 0]} intensity={0.25} />
-                    <Rotate position-y={-0.5} scale={0.2}>
-                        <Suspense fallback={<Model url={url} />}>
-                            <Model url={url} />
+                    <Rotate position-y={-0.5} scale={2}>
+                        <Suspense fallback={<Model2 url={url} />}>
+                            <Model2 url={url} />
                         </Suspense>
                     </Rotate>
                 </Canvas>
